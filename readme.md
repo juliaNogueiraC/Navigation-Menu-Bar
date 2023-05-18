ffa3595220398a6c0261b3c7f5ae6602478818b3
juliaNogueiraC <69528739+juliaNogueiraC@users.noreply.github.com>
2023-05-18 13:37:44 +0000

    Definindo as abas do menu da Nav Bar

--- readme.md
+++ readme.md
@@ -1,1 +1,1 @@
-This project is a Responsive Navigation Menu Bar, using HTML CSS and  Neumorphism UI
+This project is a Responsive Navigation Menu Bar, using HTML & CSS and Neumorphism UI.
---
+++ script.js
@@ -0,0 +1,16 @@
+//function Funcaojs(){
+  //document.getElementById("test").innerHTML = " good bye!!!";
+}
+
+//function downloadFile('/arq/arq.pdf', "arq.pdf") {
+  // Cria um objeto de URL a partir do caminho do arquivo
+ // const url = new URL(filePath, window.location.href);
+
+  // Cria um link de download
+  //const link = document.createElement("a");
+ // link.download = fileName;
+//link.href = url.createObjectURL();
+
+  // Simula um clique no link para iniciar o download
+  //link.click();
+}
